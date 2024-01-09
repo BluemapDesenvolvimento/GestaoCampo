1d715a62d0821bcbf30b01b4ef74397c42fbd0ae
Mateus Nascimento <mateusnsousa@hotmail.com>
2024-01-09 13:16:10 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Auth/TwoStepVerification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoCampo.Pages.Auth
+{
+    public class TwoStepVerificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
